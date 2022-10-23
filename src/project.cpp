2d562a5a25b7325359e82b84064d3ea1985cc4c6
Katarzyna Szymanska <katarzyna.szymanska@zoho.eu>
2022-10-23 23:01:02 +0200

    Add 'welcome phrase' to project.cpp

--- src/project.cpp
+++ src/project.cpp
@@ -4,7 +4,8 @@ int main(){
 
 int a = 0;
 int b = 0;
-
+	
+	cout<<"Welcome in simple sum program\n";
         cout<<"enter first number:\n";
                 cin>>a;
         cout<<"enter second number:\n";
